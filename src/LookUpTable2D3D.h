d38b9563c0ffa0017e9c6d5755f2e4096073dc8b
Arend Lammertink <lamare@gmail.com>
2018-04-19 11:18:50 +0200

    first commit

---
+++ src/LookUpTable2D3D.h
@@ -0,0 +1,188 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2018 Arend Lammertink
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library.  If not, see <http://www.gnu.org/licenses/>.
+//
+//-----------------------------------------------------------------------------
+// Multi-include protection
+//-----------------------------------------------------------------------------
+
+#ifndef _LOOKUP_TABLE_2D3D
+#define _LOOKUP_TABLE_2D3D
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+
+#include <Arduino.h>
+
+//-----------------------------------------------------------------------------
+// Debug stuff
+//-----------------------------------------------------------------------------
+
+//#define DEBUG            1
+
+#if DEBUG == 1
+    #define dprint(expression) Serial.print(F("# ")); Serial.print( F(#expression) ); Serial.print( F(": ") ); Serial.println( expression )
+    #define dshow(expression) Serial.println( expression )
+#else
+    #define dprint(expression) 
+    #define dshow(expression)
+#endif
+
+//-----------------------------------------------------------------------------
+// 
+// Based on:
+// https://stackoverflow.com/questions/7091294/how-to-build-a-lookup-table-in-c-sdcc-compiler-with-linear-int16_terpolation
+// http://www.cplusplus.com/forum/general/114406/
+// 
+//-----------------------------------------------------------------------------
+
+
+
+//-----------------------------------------------------------------------------
+// Linear interpolation with specialization for integer arithmetic 
+//-----------------------------------------------------------------------------
+
+template <class T>
+T interpolate( uint16_t x, uint16_t x1, uint16_t x2, T y1, T y2 )
+{
+    uint16_t  dx = x2-x1;
+    int32_t   dy = y2-y1;
+ 
+    return y1 + (x-x1) * (dy/dx);
+}
+
+
+// For integer type lookup tables, we incorporate fractions by
+// casting into an int32 and shift by 6 bits.
+
+template<>
+uint8_t interpolate( uint16_t   x, uint16_t  x1, uint16_t x2, 
+                      uint8_t  y1, uint8_t y2 )
+{
+    int32_t   _y1 = (static_cast<int32_t>(y1))<<6;
+    int32_t   _y2 = (static_cast<int32_t>(y2))<<6;
+
+    uint16_t  dx = x2-x1;
+    int32_t   dy = _y2-_y1;
+
+    dprint(F("uint8_t"));
+    
+    return y1 + (( (x-x1) * (dy/dx) )>>6);
+}
+
+
+template<>
+int8_t interpolate( uint16_t x, uint16_t x1, uint16_t x2, 
+                    int8_t  y1, int8_t y2 )
+{
+    int32_t   _y1 = (static_cast<int32_t>(y1))<<6;
+    int32_t   _y2 = (static_cast<int32_t>(y2))<<6;
+
+    uint16_t  dx = x2-x1;
+    int32_t   dy = _y2-_y1;
+
+    dprint(F("int8_t"));
+
+    return y1 + (( (x-x1) * (dy/dx) )>>6);
+}
+
+
+template<>
+uint16_t interpolate( uint16_t x, uint16_t x1, uint16_t x2, 
+                          uint16_t y1, uint16_t y2 )
+{
+    int32_t   _y1 = (static_cast<int32_t>(y1))<<6;
+    int32_t   _y2 = (static_cast<int32_t>(y2))<<6;
+
+    uint16_t  dx = x2-x1;
+    int32_t   dy = _y2-_y1;
+
+    dprint(F("uint16_t"));
+
+    return y1 + (( (x-x1) * (dy/dx) )>>6);
+}
+
+
+template<>
+int16_t interpolate( uint16_t  x, uint16_t x1, uint16_t x2, 
+                          int16_t y1, int16_t y2 )
+{
+    int32_t   _y1 = (static_cast<int32_t>(y1))<<6;
+    int32_t   _y2 = (static_cast<int32_t>(y2))<<6;
+
+    uint16_t  dx = x2-x1;
+    int32_t   dy = _y2-_y1;
+
+    dprint(F("int16_t"));
+
+    return y1 + (( (x-x1) * (dy/dx) )>>6);
+}
+
+
+
+template<int S, typename T>  // S: determine size at compile time, T: data type
+class Table2D
+{
+public:
+
+    int           size()                { return S; }
+
+    void          setXs( const uint16_t* xss ) 
+                        { memcpy( xs, xss, S*sizeof(uint16_t) ); }
+
+    void          setYs( const T* yss )
+                        { memcpy( ys, yss, S*sizeof(T) ); }
+
+#ifdef ARDUINO    // Initialize from array in PROGMEM
+    void          setXs_P( const uint16_t* xss ) 
+                        { memcpy_P( xs, xss, S*sizeof(uint16_t) ); }
+
+    void          setYs_P( const T* yss )
+                        { memcpy_P( ys, yss, S*sizeof(T) ); }
+#endif
+
+    T             getValue(uint16_t x)
+                  {    
+                    int i;
+ 
+                    if (x < xs[0])      { return ys[0];   } // minimum
+                    if (x > xs[S-1])    { return ys[S-1]; } // maximum
+
+                    /* find i, such that xs[i] <= x < xs[i+1] */
+                    for (i = 0; i < S-1; i++) {
+                        if (xs[i+1] > x) {
+                            break;
+                        }
+                    }
+
+                    return interpolate( x, xs[i], xs[i+1], ys[i], ys[i+1] );   
+                  }
+
+private:
+
+    uint16_t      xs[S];
+    T             ys[S];
+
+};
+
+
+
+
+//-----------------------------------------------------------------------------
+// 
+//-----------------------------------------------------------------------------
+
+#endif // End multi-include protection
---
+++ src/src.ino
@@ -0,0 +1,100 @@
+
+#include "LookUpTable2D3D.h"
+
+#define BAUDRATE    115200
+
+/* http://www.gammon.com.au/progmem 
+ *  
+ *  AVR Libc, and therefore Arduino provides several standard string and memory 
+ *  manipulation functions in versions that address program memory rather than RAM. 
+ *  The AVR Libc convention is to append "_P" to the name of the standard function, 
+ *  thus we have strcpy_P() and memcpy_P() which are functions used in the above 
+ *  examples to copy strings or memory from PROGMEM to RAM. The "source" argument 
+ *  is assumed to be in PROGMEM and the destination argument in RAM. Otherwise 
+ *  they function like the standard library functions strcpy() and memcpy(). 
+ *  Some useful functions of this kind are:
+
+    strcpy_P  (dest, PROGMEM src)
+    strcmp_P  (s1,   PROGMEM s2)
+    strncmp_P (s1,   PROGMEM s2,  len)
+    memcpy_P  (dest, PROGMEM src, len)
+    sprintf_P (dest, PROGMEM format, item, ... )
+ *  
+*/
+
+/* NOTE: xs MUST be sorted */
+const uint16_t xs[] PROGMEM = { 300, 700, 800, 900, 1500, 1800, 2100, 2500 };
+const uint8_t  ys[] PROGMEM = {  10,  89, 126,   0,  225,  230,  220,   10 };
+const int8_t  yss[] PROGMEM = {-127, -50, 127,   0,  10,   -30,  -50,   10 };
+
+// Some test data from Miata Brain ECU
+// https://sourceforge.net/projects/miatabrain/
+const int RPMSteps[16] PROGMEM = 
+   {   256, 512, 1024, 1536, 2048, 2560, 3072, 3584, 4086, 4598, 5120, 5632, 6144, 6656, 7168, 7680};
+
+const float tuningMap[256] PROGMEM = 
+   {   2.0, 10.0, 10.0, 28.0, 30.4, 30.4, 30.4, 36.9, 37.6, 40.9, 39.6, 31.3, 32.2, 30.0, 30.0, 30.0,
+       2.0, 10.0, 10.0, 28.0, 30.4, 30.4, 30.4, 36.9, 37.6, 40.9, 39.6, 31.3, 32.2, 30.0, 30.0, 30.0,
+       2.0, 10.0, 10.0, 28.0, 30.4, 30.4, 30.4, 36.9, 37.6, 40.9, 39.6, 31.3, 32.2, 30.0, 30.0, 30.0,
+       2.0, 10.0, 13.1, 24.2, 29.6, 29.8, 30.7, 36.0, 37.6, 39.8, 38.7, 31.3, 32.2, 30.0, 30.0, 30.0,
+       2.0, 14.0, 15.3, 22.0, 26.2, 27.3, 27.8, 31.6, 33.1, 34.4, 34.4, 32.4, 32.2, 30.0, 30.0, 30.0,
+       2.0, 12.2, 13.3, 19.8, 23.8, 24.9, 25.3, 28.9, 30.9, 32.0, 32.4, 30.2, 31.6, 30.0, 30.0, 30.0,
+       2.0,  8.7, 12.0, 18.7, 20.9, 23.3, 24.0, 27.6, 29.1, 30.2, 31.3, 28.4, 29.1, 27.6, 27.8, 27.8,
+       2.0,  5.1,  7.6, 16.9, 20.2, 21.8, 22.7, 26.0, 27.8, 29.6, 29.1, 26.2, 27.1, 26.4, 26.7, 26.7,
+       2.0,  3.3,  5.6, 15.6, 20.0, 21.3, 22.0, 24.0, 25.6, 27.6, 26.9, 25.1, 26.4, 25.3, 25.3, 25.3,
+       2.0,  1.6,  2.7, 12.2, 19.1, 20.9, 21.3, 23.3, 24.2, 25.8, 26.0, 24.0, 25.3, 24.7, 24.9, 24.9,
+       2.0, -1.3,  1.1,  8.9, 14.7, 20.2, 20.4, 22.4, 22.2, 23.1, 25.1, 23.3, 24.7, 23.8, 24.2, 24.2,
+       2.0, -2.0, -2.0,  4.9,  9.1, 11.8, 17.6, 19.1, 19.1, 22.7, 24.7, 22.7, 23.8, 24.0, 22.9, 22.9,
+       2.0, -2.0, -2.0,  4.9,  9.1, 11.8, 14.4, 19.1, 19.1, 19.6, 23.6, 21.3, 22.9, 22.9, 22.9, 22.9,
+       2.0, -2.0, -2.0,  4.9,  9.1, 11.8, 14.4, 19.1, 19.1, 19.6, 22.4, 21.3, 22.9, 22.9, 22.9, 22.9,
+       2.0, -2.0, -2.0,  4.9,  9.1, 11.8, 14.4, 19.1, 19.1, 19.6, 22.4, 21.3, 22.9, 22.9, 22.9, 22.9,
+       2.0, -2.0, -2.0,  4.9,  9.1, 11.8, 14.4, 19.1, 19.1, 19.6, 22.4, 21.3, 22.9, 22.9, 22.9, 22.9};
+
+
+
+void initSerial()
+{
+    // Open serial port with a baud rate of BAUDRATE b/s
+    Serial.begin(BAUDRATE);
+}
+
+
+void setup()
+{
+    initSerial();
+
+    Serial.println( F("Hi there") );
+  
+    Table2D<8, uint8_t>  test;
+    test.setXs_P(xs);
+    test.setYs_P(ys);
+
+    for( int idx=250; idx<2550; idx+=50)
+    {
+      uint8_t val = test.getValue(idx);
+      Serial.print(idx);
+      Serial.print( F(": ") );
+      Serial.println( (int)val );
+    }
+
+    Table2D<8, int8_t>  test2;
+    test2.setXs_P(xs);
+    test2.setYs_P(yss);
+
+    for( int idx=250; idx<2550; idx+=50)
+    {
+      int8_t val = test2.getValue(idx);
+      Serial.print(idx);
+      Serial.print( F(": ") );
+      Serial.println( (int)val );
+    }
+   
+}
+
+
+void loop()
+{
+   
+
+}
+
